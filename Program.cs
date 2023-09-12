7187c36e3fda767c5d0d5928311abf6e6c2bc341
lomstfer <90508408+lomstfer@users.noreply.github.com>
2023-09-12 12:47:35 +0200

    byte array

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+byte[] ImageToByteArray(System.Drawing.Image imageIn)
+{
+   using (var ms = new MemoryStream())
+   {
+      imageIn.Save(ms,imageIn.RawFormat);
+      return  ms.ToArray();
+   }
+}
+
+Image image = Image.FromFile("image.jpg");
+byte[] bytes = ImageToByteArray(image);
+
+for (int i = 0; i < bytes.Length; i++) {
+    Console.Write(bytes[i].ToString() + " ");
+}
