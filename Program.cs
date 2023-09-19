5b29e8978c33f87dc4c90500c625f902528d0b2b
lomstfer <90508408+lomstfer@users.noreply.github.com>
2023-09-19 21:45:46 +0200

    image ready to be processed

--- Program.cs
+++ Program.cs
@@ -1,27 +1,35 @@
-﻿using System.Drawing;
+﻿using System.Runtime.InteropServices;
+using System.Drawing;
 using System.Drawing.Imaging;
+using System.Xml.Linq;
 
-//convert bytearray to image
-System.Drawing.Image ByteArrayToImage(byte[] byteArrayIn)
-{
-   using (MemoryStream mStream = new MemoryStream(byteArrayIn))
-   {
-      return Image.FromStream(mStream);
-   }
+if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+    Console.WriteLine("only windows is supported lmao");
+    return;
 }
 
-//another easy way to convert image to bytearray
-byte[] ImgToByteConverter(Image inImg)
-{
-   ImageConverter imgCon = new ImageConverter();
-   return (byte[])imgCon.ConvertTo(inImg, typeof(byte[]));
+string filename = "image.jpg";
+
+FileStream bmpstream = new FileStream(filename, FileMode.Open, FileAccess.Read);
+Bitmap inputImage = new Bitmap(bmpstream);
+
+Rectangle rect = new Rectangle(0, 0, inputImage.Width, inputImage.Height);
+BitmapData bmpData = inputImage.LockBits(rect, ImageLockMode.ReadWrite, inputImage.PixelFormat);
+IntPtr pointeur = bmpData.Scan0;
+int bytesLength = Math.Abs(bmpData.Stride) * bmpData.Height;
+byte[] bytes = new byte[bytesLength];
+Marshal.Copy(pointeur, bytes, 0, bytesLength);
+
+ProcessBytes(ref bytes);
+
+// copy buffer back to image
+Marshal.Copy(bytes, 0, pointeur, bytes.Length);
+
+inputImage.UnlockBits(bmpData);
+
+inputImage.Save("output.jpg");
+
+
+static void ProcessBytes(ref byte[] bytes) {
+    
 }
-
-Image image = Image.FromFile("image.jpg");
-byte[] bytes = ImgToByteConverter(image);
-
-// for (int i = 0; i < bytes.Length; i++) {
-//     Console.Write(bytes[i].ToString() + " ");
-// }
-
-ByteArrayToImage(bytes).Save("output.jpg", ImageFormat.Jpeg);
