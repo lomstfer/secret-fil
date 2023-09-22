246f96d3d0763cb13f0d31c9e65f0b9cdb9c58e3
lomstfer <90508408+lomstfer@users.noreply.github.com>
2023-09-22 23:03:01 +0200

    not really working

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+using System.Text;
 
 if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
     Console.WriteLine("only windows is supported lmao");
@@ -11,16 +13,30 @@ if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
 
 string filename = "image.jpg";
 ImageByter imgb = new(filename);
-imgb.Bytes = ProcessBytes(imgb.Bytes);
-imgb.SaveFile("output.jpg");
 
-static byte[] ProcessBytes(byte[] bytes) {
-    for (int i = 0; i < bytes.Length; i++) {
-        bytes[i] = (byte)(Math.Round((float)bytes[i] / 255) * 255);
-        /*if (bytes[i] < 50)
-            bytes[i] = 0;
-        else
-            bytes[i] = 255;*/
+if (args[0] == "-write") {
+    string messageStr = File.ReadAllText("message.txt");
+    byte[] messageBytes = Encoding.ASCII.GetBytes(messageStr);
+    bool[] messageBits = new BitArray(messageBytes).Cast<bool>().ToArray();
+
+    imgb.Bits = ProcessImage(imgb.Bits, messageBits);
+
+    imgb.SaveFile("output.jpg");
+
+    static BitArray ProcessImage(BitArray imageBits, bool[] messageBits) {
+        Console.WriteLine(messageBits.Length);
+        for (int i = 0; i < messageBits.Length; i++) {
+            imageBits.Set(i, messageBits[i]);
+        }
+
+        return imageBits;
     }
-    return bytes;
+}
+else if (args[0] == "-read") {
+    Console.WriteLine("the message is:");
+    Console.WriteLine(GetMessage(imgb.Bytes));
+
+    static string GetMessage(byte[] imageBytes) {
+        return BitConverter.ToString(imageBytes);
+    }
 }
