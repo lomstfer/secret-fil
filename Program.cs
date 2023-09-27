41307ec90a0b03e3425b0cc2cf42c3736ebf3895
lomstfer <90508408+lomstfer@users.noreply.github.com>
2023-09-27 10:10:25 +0200

    least sign bit

--- Program.cs
+++ Program.cs
@@ -14,16 +14,16 @@ if (args[0] == "-write") {
     string filename = "image.png";
     ImageByter imgb = new(filename);
 
-    // // debug random message. example -write 20000. 20000 random bytes as message
-    // Random random = new();
-    // byte[] testOutputBytes = new byte[int.Parse(args[1])];
-    // random.NextBytes(testOutputBytes);
-    // File.WriteAllText("message.txt", Encoding.ASCII.GetString(testOutputBytes));
-    // // --
+    // debug random message. example -write 20000. 20000 random bytes as message
+    Random random = new();
+    byte[] testOutputBytes = new byte[int.Parse(args[1])];
+    random.NextBytes(testOutputBytes);
+    File.WriteAllText("message.txt", Encoding.ASCII.GetString(testOutputBytes));
+    // --
 
     string messageStr = File.ReadAllText("message.txt");
     byte[] messageBytes = Encoding.ASCII.GetBytes(messageStr);
-    if (messageBytes.Length > imgb.Bytes.Length) {
+    if (messageBytes.Length * 8 > imgb.Bytes.Length) {
         Console.WriteLine("Message too long / image too small. No message was put in the image :(");
         return;
     }
@@ -36,24 +36,22 @@ if (args[0] == "-write") {
     static BitArray ProcessImage(BitArray imageBits, BitArray messageBits, int messageBytesLength) {
         BitArray messageByteLengthBits = new BitArray(BitConverter.GetBytes(messageBytesLength));
 
-        if (messageBits.Length + messageByteLengthBits.Length > imageBits.Length) {
+        if ((messageBits.Length + messageByteLengthBits.Length) * 8 > imageBits.Length) {
             Console.WriteLine("Message too long / image too small. No message was put in the image :(");
             return imageBits;
         }
 
         int i = 0;
-        while (i < messageByteLengthBits.Length) {
-            imageBits.Set(i, messageByteLengthBits.Get(i));
+        for (int j = 0; j < messageByteLengthBits.Length; j++) {
+            imageBits.Set(i, messageByteLengthBits.Get(j));
 
-            i += 1;
+            i += 8;
         }
 
-        int j = 0;
-        while (j < messageBits.Length) {
+        for (int j = 0; j < messageBits.Length; j++) {
             imageBits.Set(i, messageBits.Get(j));
 
-            i += imageBits.Length / messageBits.Length;
-            j += 1;
+            i += 8/* imageBits.Length / messageBits.Length */;
         }
 
         return imageBits;
@@ -72,15 +70,20 @@ else {
 }
 
 static string GetMessage(byte[] imageBytes, BitArray imageBits) {
-    // how many bytes the message is
-    int messageByteLength = BitConverter.ToInt32(new byte[]{imageBytes[0], imageBytes[1], imageBytes[2], imageBytes[3]});
+    BitArray messageByteLengthBits = new(32);
+    int j = 0;
+    for (int i = 0; i < 32; i++) {
+        messageByteLengthBits.Set(i, imageBits.Get(j));
+        j += 8;
+    }
+
+    int messageByteLength = BitConverter.ToInt32(ImageByter.BitArrayToByteArray(messageByteLengthBits));
 
     BitArray output = new(messageByteLength * 8);
 
-    int j = 32; // skipping the messageByteLength bite
     for (int i = 0; i < output.Length; i += 1) {
         output.Set(i, imageBits.Get(j));
-        j += imageBits.Length / output.Length;
+        j += 8/* imageBits.Length / output.Length */;
     }
 
     return Encoding.ASCII.GetString(ImageByter.BitArrayToByteArray(output));
